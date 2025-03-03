684ce4a0553d8a7b8e1612638490499de631856f
nitinshk <kodansharma77@gmail.com>
2025-03-03 09:45:33 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Nav from "./components/nav";
+import Hero from "./components/hero";
+const App =()=>{
+return (
+  <div>
+    <Nav/>
+    <Hero/>
+  </div>
+);
+};
+export default App;
